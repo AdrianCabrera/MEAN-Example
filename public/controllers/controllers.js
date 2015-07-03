db96dd96f703a97aa47d9b869510677e37618af6
Adrian Cabrera <11042779+AdrianCabrera@users.noreply.github.com>
2015-07-03 17:23:05 +0100

    Get data from server

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -2,7 +2,18 @@
  * [myApp Angular module]
  * @angular.module {[angular.module]}
  */
-var myApp = angular.module('myApp', [])
-	.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-		console.log("Hello World from controller");
-	}]);
+ var myApp = angular.module('myApp', [])
+
+ /**
+  * First controller
+  * @param  {[type]} $scope    [Scope of angular]
+  * @param  {Object} $http     [To communicate with the server]
+  */
+  .controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+
+  	$http.get("/contactList").success(function(response){
+  		console.log("I got the data I requested");
+  		$scope.contactList = response;
+  	});
+
+  }]);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var app = express();
 var port=3000;
 
@@ -7,3 +8,30 @@ app.use(express.static(__dirname+"/public"));
 app.listen(port,function(){
 	console.log('Server runing on port: '+port);
 })
+
+
+
+app.get("/contactList",function(req,res){
+	console.log("I received a GET request");
+	
+  	person1={
+  		name:"Adri1",
+  		email:"adrian1@test.com",
+  		number:"111"
+  	};
+  	person2={
+  		name:"Adri2",
+  		email:"adrian2@test.com",
+  		number:"222"
+  	};
+  	person3={
+  		name:"Adri3",
+  		email:"adrian3@test.com",
+  		number:"333"
+  	};
+
+  	var contactList=[person1,person2,person3];
+  	
+	res.json(contactList);
+
+});
