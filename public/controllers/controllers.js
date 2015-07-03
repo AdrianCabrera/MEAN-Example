789c8be93c10802aabdcff02981b7fa41f8b0078
Adrian Cabrera <11042779+AdrianCabrera@users.noreply.github.com>
2015-07-03 19:27:48 +0100

    Fixes, performance and usability

--- public/controllers/controllers.js
+++ public/controllers/controllers.js
@@ -11,49 +11,70 @@
   */
   .controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
-  	var refresh=function(){  	
-  		$http.get("/contactList").success(function(response){
-  			console.log("I got the data I requested");
-  			$scope.contactList = response;
-  			$scope.contact = "";
-  		});
-  	};
-
-
-  	refresh();
+  	$http.get("/contactList").success(function(response){
+  		console.log("I got the data I requested");
+  		$scope.contactList = response;
+  		$scope.clear();
+  	});
   	
 
   	$scope.addContact=function(){
   		console.log($scope.contact);
-  		$http.post("/contactList",$scope.contact).success(function(response){
-  			console.log(response);
-  			refresh();
-  		});
+  		if($scope.contact){
+  			$http.post("/contactList",$scope.contact).success(function(response){
+  				console.log(response);
+  				$scope.contactList.push(response);
+  				$scope.clear();
+  			});
+  		}
   	};
 
   	$scope.remove=function(id){
   		console.log(id);
   		$http.delete("/contactList/" + id).success(function(response){
-  			console.log(response);
-  			refresh();
+  			console.log(response);  			
+  			for (var i = 0; i < $scope.contactList.length; i++) {
+  				if($scope.contactList[i]._id == id){
+  					$scope.contactList.splice(i, 1);
+  					break;
+  				}
+  			}
+  			$scope.clear();
   		});
   	};
 
   	$scope.edit=function(id){
   		console.log(id);
-  		$http.get("/contactList/" + id).success(function(response){
-  			$scope.contact = response;
-  			console.log(response);
-  		});
+  		// $http.get("/contactList/" + id).success(function(response){
+  		// 	$scope.contact = response;
+  		// 	console.log(response);
+  		// });
+
+  		//Within the app, it's not necessary to call the server
+  		var contact = $scope.contactList.filter(function(item) {
+  			return item._id === id;
+  		})[0];
+  		$scope.contact = JSON.parse(JSON.stringify(contact)); 
+
   	};
 
   	$scope.update=function(){
   		console.log($scope.contact._id);
   		$http.put("/contactList/" + $scope.contact._id,$scope.contact).success(function(response){
   			console.log(response);
-  			refresh();
+
+  			//Already on success, work within the app, it's not necessary to call the server
+  			for (var i = 0; i < $scope.contactList.length; i++) {
+  				if($scope.contactList[i]._id == $scope.contact._id){
+  					$scope.contactList.splice(i, 1,response);
+  					// $scope.contactList.push();
+  					break;
+  				}
+  			}  
+  			$scope.clear();
   		});
   	};
+
   	$scope.clear=function(){
   		$scope.contact = "";
   	};
